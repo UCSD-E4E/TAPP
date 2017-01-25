838a6bede84e7c59b68baf056ffd6558418626be
Matt Epperson <m.p.epperson@gmail.com>
2017-01-25 11:52:40 -0800

    UTILS: Cleaned up the utils and added tif2ply

--- utils/gdal_utils.py
+++ utils/gdal_utils.py
@@ -1,9 +1,10 @@
+from plyfile import PlyData, PlyElement
 from osgeo import gdal
 import subprocess
-
-# GDAL affine transform parameters, According to gdal
-# documentation xoff/yoff are image left corner, a/e are pixel wight/height
-# and b/d is rotation and is zero if image is north up.
+import numpy as np
+import scipy
+from scipy import ndimage
+import sys
 
 
 def pixel2coord(gt, x, y):
@@ -16,14 +17,25 @@ def pixel2coord(gt, x, y):
 
 def coord2pixel(gt, lat, lon):
     """Transforms lat/lon coordinates to pixel coordinates"""
-    x = int(round((lat-gt[0])/gt[1]))
-    y = int(round((lon-gt[3])/gt[5]))
+    x = int(round((lon-gt[0])/gt[1]))
+    y = int(round((lat-gt[3])/gt[5]))
 
     return x, y
 
 
 def trim(lat1, lon1, lat2, lon2, infile, outfile):
     '''
+    Calls gdal_translate using the provided arguments to trim a larger tif
+    file into a smaller one based on the upper left and lower right lat/lon
+    pairs.
+
+        Args:
+            lat1, lon1 (float): Upper left corner lat/lon
+
+            lat2, lon2 (float): Lower right corner lat/lon
+
+        Return:
+            returncode (int): Return code from calling gdal_translate
 
     '''
     transform = gdal.Open(infile).GetGeoTransform()
@@ -34,18 +46,101 @@ def trim(lat1, lon1, lat2, lon2, infile, outfile):
     args = ['gdal_translate', '-srcwin']
     args.extend([str(x1), str(y1), str(x2-x1), str(y2-y1), infile, outfile])
 
-    ls_output = subprocess.call(args)
-    print(ls_output)
+    try:
+        res = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    except Exception as ex:
+        res = ex.returncode, str(ex.output)
+
+    return res
+
+
+def tif2mesh(tiffile, plyfile, upsample, interpolation):
+    """
+    Turns a tif file into a ply mesh file
+
+    Args:
+        tiffile (string): File name of the tif we are converting
+
+        plyfile (string): Outputfile name
+
+        upsample (int): The magnitude which to upsample the grid, i.e. a
+            sample rate of 2 will effectively square the size image.
+
+        interpolation (int): The method by which to interpolate points if we
+            are upsampling. (0 = Nearest, 1 = Bilinear, 3 = Cubic). Nominally
+            there are 5 different orders but I can't find the documentation on
+            what they are.
+
+    """
+    tif = gdal.Open(tiffile)
+    tf = tif.GetGeoTransform()
+
+    faces = []
+    cnt = 0
+
+    # Get image x,y,z
+    channel = np.array(tif.GetRasterBand(1).ReadAsArray()).astype(np.float32)
+
+    # Upsample the image with cubic interpolation
+    channel = scipy.ndimage.zoom(channel, upsample, order=interpolation)
+
+    # Create our point cloud
+    # TODO: We need to keep in mind that the pixel size should change with
+    # upsampling
+    points = [(col*tf[1], row*tf[5], channel[row][col])
+              for row in range(channel.shape[0])
+              for col in range(channel.shape[1])]
+
+    print(len(points))
+
+    # Create faces from grid. Basically this is just turning  a grid into
+    # triangles
+    for row in range(channel.shape[0]-1):
+        for col in range(channel.shape[1]):
+            if col is not channel.shape[1]-1:
+                v1 = cnt
+                v2 = cnt + 1
+                v3 = cnt + channel.shape[1] + 1
+                v4 = cnt + channel.shape[1]
+
+                faces.append(([v1, v2, v3], 0, 0, 255))
+                faces.append(([v1, v4, v3], 0, 0, 255))
+
+            cnt = cnt + 1
+    print cnt
+
+    # Create np array for turning vertices and faces into a ply file
+    faces = np.array(faces, dtype=[('vertex_indices', 'i4', (3,)),
+                                   ('red', 'u1'),
+                                   ('green', 'u1'),
+                                   ('blue', 'u1')])
+
+    vertices = np.array(points, dtype=[('x', 'f4'),
+                                       ('y', 'f4'),
+                                       ('z', 'f4')])
+
+    # Write data to new file
+    el = PlyElement.describe(vertices, 'vertex')
+    el2 = PlyElement.describe(faces, 'face')
+
+    PlyData([el, el2], text=True).write(plyfile)
+
 
 if __name__ == "__main__":
-    data_set = gdal.Open('../data/canyon_dem_utm11.tif')
-    gt = data_set.GetGeoTransform()
+    tif2mesh("../data/tif/black_mtn_alos.tif",
+             "../data/ply/black_mtn_alos.ply",
+             int(sys.argv[1]), 3)
 
-    lat1, lon1 = pixel2coord(gt, 0, 0)
-    lat2, lon2 = pixel2coord(gt, 10, 10)
-    trim(lat1, lon1, lat2, lon2, '../data/canyon_dem_utm11.tif', '../data/test.tif')
-
-    print(pixel2coord(gt, 1, 1))
-    print(coord2pixel(gt, 478279.930, 3638571.112))
-    x, y = pixel2coord(gt, 100, 1220)
-    print(coord2pixel(gt, x, y))
+    # data_set = gdal.Open('../data/canyon_dem_utm11.tif')
+    # gt = data_set.GetGeoTransform()
+    #
+    # lat1, lon1 = pixel2coord(gt, 0, 0)
+    # lat2, lon2 = pixel2coord(gt, 10, 10)
+    #
+    # trim(lat1, lon1, lat2, lon2, '../data/canyon_dem_utm11.tif',
+    #      '../data/test.tif')
+    #
+    # print(pixel2coord(gt, 1, 1))
+    # print(coord2pixel(gt, 478279.930, 3638571.112))
+    # x, y = pixel2coord(gt, 100, 1220)
+    # print(coord2pixel(gt, x, y))
--- utils/ply_utils.py
+++ utils/ply_utils.py
@@ -7,15 +7,7 @@ import numpy.lib.recfunctions as rfn
 # file
 
 
-def ply_face2vertices(face, vertices):
-    vert1 = np.asarray([vert for vert in vertices[face[0]]])
-    vert2 = np.asarray([vert for vert in vertices[face[1]]])
-    vert3 = np.asarray([vert for vert in vertices[face[2]]])
-
-    return vert1, vert2, vert3
-
-
-def ply_colorize(filename):
+def colorize(filename):
     """
     This function takes a ply that does not contain colored faces and convert
     it to a colored file format. Note that if you want to work with the colors
@@ -58,71 +50,71 @@ def ply_colorize(filename):
     return True
 
 
-def ply_add_properties(filename, element, properties, value):
-    """
-    This function takes a ply file and attempts to add properties to an
-    elements. One common example is adding colors to the face elements or
-    adding a counter.
-
-    Example:
-
-    Args:
-        element (string): The name of the element you are adding a property to.
-
-        properties ([] string): The names of the properties you adding. It
-        should come in the form of a list of tuples i.e. ('red', 'u1')
-
-        value (float): Default value for the propertie to start with.
-
-    Returns:
-        boolean: Returns true if the properties we successfully added false
-        otherwise.
-
-    """
-
-    # Read data from existing ply
-    plydata = PlyData.read(filename)
-
-    # Return false if the file already contains colored faces
-    # TODO FIX
-    # if set(properties) < set(str(plydata[element].properties)):
-    #     return False
-
-    # TODO This only covers these two types of elements
-    faces = plydata['face'].data
-    vertices = plydata['vertex'].data
-
-    # Create new set of faces with color attribute
-    # NOTE: This may take a while for large mesh files
-    new_faces = []
-    for face in faces:
-        new_face = [face[0].tolist()]
-        new_face.extend([value for prop in properties])
-        new_faces.append(tuple(new_face))
-
-    print(faces.dtype.fields, faces.dtype.names[0])
-    # for idx in range(len(faces.dtype.names)):
-        # print(faces.dtype.names[idx], faces.dtype.fields[idx])
-
-    data_types = []
-    for t in range(len(faces.dtype)):
-        print(t, faces.dtype[t])
-        data_types.append(faces.dtype[t])
-
-    print(data_types)
-    data_types.extend(properties)
-
-    # TODO Figure out how to set the properities here
-    faces = np.array(new_faces, dtype=data_types)
-    # faces = np.array(new_faces, dtype=[('vertex_indices', 'i4', (3,)),
-    #                                ('red', 'u1'),
-    #                                ('green', 'u1'),
-    #                                ('blue', 'u1')])
-
-    # Write data to new file
-    el = PlyElement.describe(vertices, 'vertex')
-    el2 = PlyElement.describe(faces, 'face')
-
-    PlyData([el, el2], text=True).write(filename.strip(".ply")+"_colored.ply")
-
-    return True
+# def ply_add_properties(filename, element, properties, value):
+#     """
+#     This function takes a ply file and attempts to add properties to an
+#     elements. One common example is adding colors to the face elements or
+#     adding a counter.
+#
+#     Example:
+#
+#     Args:
+#         element (string): The name of the element you are adding a property to.
+#
+#         properties ([] string): The names of the properties you adding. It
+#         should come in the form of a list of tuples i.e. ('red', 'u1')
+#
+#         value (float): Default value for the propertie to start with.
+#
+#     Returns:
+#         boolean: Returns true if the properties we successfully added false
+#         otherwise.
+#
+#     """
+#
+#     # Read data from existing ply
+#     plydata = PlyData.read(filename)
+#
+#     # Return false if the file already contains colored faces
+#     # TODO FIX
+#     # if set(properties) < set(str(plydata[element].properties)):
+#     #     return False
+#
+#     # TODO This only covers these two types of elements
+#     faces = plydata['face'].data
+#     vertices = plydata['vertex'].data
+#
+#     # Create new set of faces with color attribute
+#     # NOTE: This may take a while for large mesh files
+#     new_faces = []
+#     for face in faces:
+#         new_face = [face[0].tolist()]
+#         new_face.extend([value for prop in properties])
+#         new_faces.append(tuple(new_face))
+#
+#     print(faces.dtype.fields, faces.dtype.names[0])
+#     # for idx in range(len(faces.dtype.names)):
+#         # print(faces.dtype.names[idx], faces.dtype.fields[idx])
+#
+#     data_types = []
+#     for t in range(len(faces.dtype)):
+#         print(t, faces.dtype[t])
+#         data_types.append(faces.dtype[t])
+#
+#     print(data_types)
+#     data_types.extend(properties)
+#
+#     # TODO Figure out how to set the properities here
+#     faces = np.array(new_faces, dtype=data_types)
+#     # faces = np.array(new_faces, dtype=[('vertex_indices', 'i4', (3,)),
+#     #                                ('red', 'u1'),
+#     #                                ('green', 'u1'),
+#     #                                ('blue', 'u1')])
+#
+#     # Write data to new file
+#     el = PlyElement.describe(vertices, 'vertex')
+#     el2 = PlyElement.describe(faces, 'face')
+#
+#     PlyData([el, el2], text=True).write(filename.strip(".ply")+"_colored.ply")
+#
+#     return True
