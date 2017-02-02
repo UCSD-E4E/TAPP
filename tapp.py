b04fd61c9453163bec62c7e2e3343ec3be443ae2
Matt Epperson <m.p.epperson@gmail.com>
2017-02-02 07:25:43 -0800

    TAPP: Updated our main/data/params to utm
    Created a new search area file and varified that gdal_utils was
    correctly trimming it by manually selecting a feature, trimming,
    than comparing the two raster layers in qgis. Update the poses
    file to use utm and gave it the corners of the trimmed tif. Also
    snuck in some minor styling changes to tapp.py

--- tapp.py
+++ tapp.py
@@ -34,26 +34,32 @@ def read_meta_file(filename, tag):
 
 def main():
     """Main script logic"""
-    parser = argparse.ArgumentParser(
-        description = 'TAPP Camera Pose Coverage Analysis')
-    parser.add_argument('geotiff', action='store', type=str, 
-        help='GeoTIFF DEM for the search area')
-    # Format should be XX.XXXXX, YY.YYYYY, northwestern corner first line, 
+    parser = argparse.ArgumentParser(description='TAPP Camera Pose Coverage'
+                                     'Analysis')
+
+    parser.add_argument('geotiff', action='store', type=str,
+                        help='GeoTIFF DEM for the search area')
+
+    # Format should be XX.XXXXX, YY.YYYYY, northwestern corner first line,
     #   southwestern corner second line
     # TODO upgrade to ESRI shapefile in future
-    parser.add_argument('search_area', action='store', type=str, 
-        help='Text file containing the northwestern and southwestern '
-        'coordinates of the search area in decimal degrees, comma separated')
+    parser.add_argument('search_area', action='store', type=str,
+                        help='Text file containing the northwestern and '
+                        'southwestern coordinates of the search area in'
+                        ' decimal degrees, comma separated')
+
     # Format should be TAG: VALUE
     # Tags should be X_RES, Y_RES, FOV, assuming square pixels!
-    parser.add_argument('camera_params', action='store', type=str, 
-        help='Text file containing the tagged camera parameters')
-    # Format should be a CSV file with the following columns with headers: 
+    parser.add_argument('camera_params', action='store', type=str,
+                        help='Text file containing the tagged camera '
+                        'parameters')
+
+    # Format should be a CSV file with the following columns with headers:
     #   LAT, LON, ALT, ROLL, PITCH, YAW
     # Yaw angle should be specified relative to true north
     # Altitude should be in meters AGL
-    parser.add_argument('poses', action='store', type=str, 
-        help='CSV file containing the camera poses')
+    parser.add_argument('poses', action='store', type=str,
+                        help='CSV file containing the camera poses')
 
     args = parser.parse_args()
 
@@ -65,12 +71,13 @@ def main():
     # Trim tiff
     print("Trimming tiff")
     target_area = np.genfromtxt(target_area_file, delimiter=',')
-    retval = gdal_utils.trim(target_area[0][1], target_area[0][0], 
-        target_area[1][1], target_area[1][0], geotiff_file, '.tmp.tiff')
+    retval = gdal_utils.trim(target_area[0], target_area[1],
+                             geotiff_file, 'tmp.tiff')
+
     # Load tiff to tiff->ply generator
     print("Tiff -> Mesh")
-    retval = gdal_utils.tif2mesh('.tmp.tiff', '.tmp.ply', 1, 0)
-    ply_file = '.tmp.ply'
+    retval = gdal_utils.tif2mesh('tmp.tiff', 'tmp.ply', 1, 0)
+    ply_file = 'tmp.ply'
 
     # Load camera params
     print("Loading camera params")
@@ -81,20 +88,27 @@ def main():
 
     # Load camera poses
     print("Loading camera poses")
-    poses = np.loadtxt(poses_file, delimiter=",", 
-        dtype={ 'names': ('LAT', 'LON', 'ALT', 'ROLL', 'PITCH', 'YAW'), 
-                'formats': ('f8', 'f8', 'f8', 'f8', 'f8', 'f8')}, skiprows=1)
+
+    # Setup dtype
+    params = ('EASTING', 'NORTHING', 'ALT', 'ROLL', 'PITCH', 'YAW')
+    formats = ('f8', 'f8', 'f8', 'f8', 'f8', 'f8')
+
+    poses = np.loadtxt(poses_file, delimiter=",", dtype={'names': params,
+                       'formats': formats}, skiprows=1)
+
     # Transform pose to local frame!
     tif = gdal.Open(geotiff_file)
     tf = tif.GetGeoTransform()
+
     print("Processing poses")
     for pose in poses:
-        x, y = gdal_utils.coord2pixel(tf, pose['LAT'], pose['LON'])
-        pose['LAT'] = x
-        pose['LON'] = y
-        print("%f, %f, %f, %f, %f, %f" % (pose['LAT'], pose['LON'], 
-            pose['ALT'], pose['ROLL'], pose['PITCH'], pose['YAW']))
+        px, py = gdal_utils.coord2pixel(tf, pose['EASTING'], pose['NORTHING'])
+
+        print("Pixel X: %f, Pixel Y: %f, Alt: %f, Roll %f, Pitch %f, Yaw %f" %
+              (px, py, pose['ALT'], pose['ROLL'], pose['PITCH'], pose['YAW']))
+
         camera.snap(pose)
+
     print("Finished processing")
     camera.closePly()
 
