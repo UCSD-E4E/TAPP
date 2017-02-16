b1e98dfc75f6e0b29d624e04d45818115979c8c6
Nathan Hui <ntlhui@gmail.com>
2017-02-15 21:54:52 -0800

    Added code to read alt out from LRF

---
+++ realtime/lrf_reader.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import serial
+import pymavlink
+
+def main():
+	port = serial.Serial("/dev/ttyUSB0", baudrate = 115200, timeout = 3.0)
+	while True:
+		line = port.readline()
+		alt = float(line.split()[0])
+		if alt == 130.00:
+			print("noval")
+		else:
+			print(alt)
+
+
+if __name__ == '__main__':
+	main()
